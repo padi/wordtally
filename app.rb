f2bc209362c819162b4c10630972dacefd6f0ce4
Marc Ignacio <marcrendlignacio@gmail.com>
2013-07-25 21:52:45 +0800

    add results table

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require 'sinatra'
+require 'ostruct'
 
 get '/' do
+  @results = [OpenStruct.new(word: 'example', count: 1),
+              OpenStruct.new(word: 'another', count: 1)]
   haml :index, format: :html5
 end
 
 post '/' do
   # TODO:
   # - should feed text into TalliesWords
-  # - give results table for word count e.g.
-  #|   word    |  count   |
-  #| something |    20    |
-  #|  another  |    10    |
-  @results = params['text']
+  # - replace results into actual results :P
+  @results = [OpenStruct(word: 'example', count: 1),
+              OpenStruct(word: 'another', count: 1)]
+  @debug = params['text']
   haml :index, format: :html5
 end
